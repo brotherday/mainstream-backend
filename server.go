634922bfe3e165d86755eadae4d039c9d4b16f53
Steve Huguenin-Elie <steve.huguenin-elie@protonmail.ch>
2023-02-05 20:47:27 +0000

    HTTP Routing

--- server.go
+++ server.go
@@ -1,26 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    r := mux.NewRouter()
+	r := mux.NewRouter()
 
-    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-    })
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
 
-    r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-        vars := mux.Vars(r)
-        title := vars["title"]
-        page := vars["page"]
+	r.HandleFunc("/store", func(w http.ResponseWriter, r *http.Request) {
+		file := r.PostFormValue("file")
 
-        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
-    })
+		fmt.Fprintf(w, "%s\n", file)
+	})
 
-    http.ListenAndServe(":80", r)
+	r.HandleFunc("/retrieve/{epoch}/{num}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		epoch, num := vars["epoch"], vars["num"]
+
+		fmt.Fprintf(w, "You've requested the presentation %s/%s\n", epoch, num)
+	})
+
+	http.ListenAndServe(":3000", r)
 }
